8fe77477c5fff52230358b191f8f4d545d39bb28
Jota Farias <fariasjota09@gmail.com>
2023-03-12 21:53:38 -0300

    Update fetch_profile_data

--- fetch_profile_data.py
+++ fetch_profile_data.py
@@ -1,40 +1,80 @@
 import time
 
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+from utils import (
+    DATABASE_PATH,
+    create_connection,
+    create_driver,
+    execute_query_return,
+    get_competencias_from_usuario,
+    get_experiencias_from_usuario,
+    get_id_usuario,
+    get_idiomas_from_usuario,
+    get_interesses_from_usuario,
+    insert_competencia,
+    insert_experiencia,
+    insert_idioma,
+    insert_interesses,
+    linkedin_login,
+)
 
-from config import LINKEDIN_LOGIN, LINKEDIN_PASSWORD
+db = create_connection(DATABASE_PATH)
+select_usarios = """
+SELECT url FROM usuarios
+"""
+usuarios_url = execute_query_return(db, select_usarios)
+db.close()
 
-profile_urls = []
-with open("profile_urls.txt", "r") as file:
-    for f in file:
-        profile_urls.append(f)
+usuarios_url = [user_url[0] for user_url in usuarios_url]
+usuarios_idioma = [pu + "/details/languages" for pu in usuarios_url]
+usuarios_competencia = [pu + "/details/skills" for pu in usuarios_url]
+usuarios_interesse = [pu + "/details/interests" for pu in usuarios_url]
+usuarios_experiencia = [pu + "/details/experience" for pu in usuarios_url]
 
 
-service = ChromeService(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-
-LINKEDIN_URL = "https://www.linkedin.com/"
-driver.get(LINKEDIN_URL)
-
+driver = create_driver()
+driver = linkedin_login(driver)
 time.sleep(1)
 
-username = driver.find_element(By.ID, "session_key")
-username.send_keys(LINKEDIN_LOGIN)
+db = create_connection(DATABASE_PATH)
 
-password = driver.find_element(By.ID, "session_password")
-password.send_keys(LINKEDIN_PASSWORD)
+# for user_url, user_idioma, user_comp, user_inter, user_exp in zip(usuarios_url, usuarios_idioma, usuarios_competencia, usuarios_interesse, usuarios_experiencia):
+TOTAL = 100
+for user_url, user_idioma, user_comp, user_inter, user_exp in zip(
+    usuarios_url[:TOTAL],
+    usuarios_idioma[:TOTAL],
+    usuarios_competencia[:TOTAL],
+    usuarios_interesse[:TOTAL],
+    usuarios_experiencia[:TOTAL],
+):
+    id_usuario = get_id_usuario(db, user_url)
 
-login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-login_button.click()
+    driver.get(user_idioma)
+    time.sleep(2)
+    content = driver.page_source
+    langs_names_levels = get_idiomas_from_usuario(content)
+    if langs_names_levels is not None:
+        insert_idioma(db, langs_names_levels, id_usuario)
 
-time.sleep(1)
+    driver.get(user_comp)
+    time.sleep(2)
+    content = driver.page_source
+    competencias = get_competencias_from_usuario(content)
+    if competencias is not None:
+        insert_competencia(db, competencias, id_usuario)
 
-driver.get(profile_urls[0])
-content = driver.page_source
+    driver.get(user_inter)
+    time.sleep(2)
+    content = driver.page_source
+    interesses = get_interesses_from_usuario(content)
+    if interesses is not None:
+        insert_interesses(db, interesses, id_usuario)
 
-with open("profile1.html", "w") as file:
-    file.write(content)
+    driver.get(user_exp)
+    time.sleep(2)
+    content = driver.page_source
+    experiencias = get_experiencias_from_usuario(content)
+    if experiencias is not None:
+        insert_experiencia(db, experiencias, id_usuario)
+
+driver.quit()
+db.close()
