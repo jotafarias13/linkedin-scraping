6e7dfaaa727a650cded33b9646e5ef93140ebe77
Jota Farias <fariasjota09@gmail.com>
2023-03-12 21:53:28 -0300

    Update create_db

--- create_db.py
+++ create_db.py
@@ -1,35 +1,19 @@
-import sqlite3
-from sqlite3 import Error
+from utils import DATABASE_PATH, create_connection, execute_query
 
-
-def create_connection(path_to_file: str) -> sqlite3.Connection:
-    connection = None
-
-    try:
-        connection = sqlite3.connect(path_to_file)
-        print(f"Conexão com o banco {path_to_file} foi realizada")
-    except Error as e:
-        print(f"Erro: {e}")
-
-    return connection
-
-
-def execute_query(connection: sqlite3.Connection, query: str) -> None:
-    cursor = connection.cursor()
-
-    try:
-        cursor.execute(query)
-        connection.commit()
-    except Error as e:
-        print(f"Erro: {e}")
-
+create_usuarios_table = """
+CREATE TABLE IF NOT EXISTS usuarios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  url TEXT NOT NULL
+);
+"""
 
 create_linguas_table = """
 CREATE TABLE IF NOT EXISTS linguas (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   id_pessoa INTEGER NOT NULL,
   lingua TEXT NOT NULL,
-  nivel TEXT NOT NULL
+  nivel TEXT NOT NULL,
+  FOREIGN KEY (id_pessoa) REFERENCES usuarios (id)
 );
 """
 
@@ -37,14 +21,17 @@ create_competencias_table = """
 CREATE TABLE IF NOT EXISTS competencias (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   id_pessoa INTEGER NOT NULL,
-  competencia TEXT NOT NULL
+  competencia TEXT NOT NULL,
+  FOREIGN KEY (id_pessoa) REFERENCES usuarios (id)
 );
 """
 
-create_empresas_table = """
-CREATE TABLE IF NOT EXISTS empresas (
+create_interesses_table = """
+CREATE TABLE IF NOT EXISTS interesses (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  empresa TEXT NOT NULL
+  id_pessoa INTEGER NOT NULL,
+  interesse TEXT NOT NULL
+  FOREIGN KEY (id_pessoa) REFERENCES usuarios (id)
 );
 """
 
@@ -52,16 +39,20 @@ create_experiencias_table = """
 CREATE TABLE IF NOT EXISTS experiencias (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   id_pessoa INTEGER NOT NULL,
-  id_empresa INTEGER NOT NULL,
+  empresa INTEGER NOT NULL,
   data_comeco TEXT NOT NULL,
-  data_fim TEXT NOT NULL
+  data_fim TEXT NOT NULL,
+  FOREIGN KEY (id_pessoa) REFERENCES usuarios (id)
 );
 """
 
 
-db = create_connection("linkedin_scraping.sqlite3")
+db = create_connection(DATABASE_PATH)
 
+execute_query(db, create_usuarios_table)
 execute_query(db, create_linguas_table)
 execute_query(db, create_competencias_table)
-execute_query(db, create_empresas_table)
+execute_query(db, create_interesses_table)
 execute_query(db, create_experiencias_table)
+
+db.close()
